50dd765e8dcdce93846edc22d81052e5bca7e6c7
NgoVietCuong <ngocuong972000@gmail.com>
2023-12-09 17:05:39 +0700

    apply jwt to request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const next = require("next");
+const jwt = require("jsonwebtoken");
 const koa = require("koa");
+const axios = require("axios");
 const koaRouter = require("koa-router");
 const koaSession = require("koa-session");
 const { createShopifyAuth, verifyRequest } = require("simple-koa-shopify-auth");
@@ -11,7 +13,11 @@ const {
   SHOPIFY_SCOPES,
   API_VERSION,
   APP_DOMAIN,
-  APP_PORT
+  APP_PORT,
+  SERVER_URL,
+  JWT_ALGORITHM,
+  JWT_EXPIRES_IN,
+  JWT_SECRET_KEY,
 } = process.env;
 
 Shopify.Context.initialize({
@@ -40,17 +46,74 @@ app.prepare().then(() => {
   server.use(
     createShopifyAuth({
       accessMode: "offline",
+      authRoute: "/install/auth",
       async afterAuth(ctx) {
+        console.log(ctx.state.shopify)
         const { shop, accessToken } = ctx.state.shopify;
-        const { host } = ctx.query;
-        console.log("shop", shop);
-        console.log("accessToken", accessToken);
+        const jwtPayLoad = {
+          shopDomain: shop,
+          accessToken: accessToken
+        }
+        const jwtSecretKey = JWT_SECRET_KEY;
+        const jwtOptions = {
+          algorithm: JWT_ALGORITHM,
+          expiresIn: JWT_EXPIRES_IN
+        }
+
+        const jwtToken =  jwt.sign(jwtPayLoad, jwtSecretKey, jwtOptions);
+
         ctx.cookies.set("shop", shop, { httpOnly: false, secure: true, sameSite: "none" });
         ctx.cookies.set("accessToken", accessToken, { httpOnly: false, secure: true, sameSite: "none" });
-        ctx.redirect("/");
+        ctx.cookies.set("jwtToken", jwtToken, { httpOnly: false, secure: true, sameSite: "none" });
+
+        let shopAuthentication = true;
+
+        try {
+          const shopRes = await axios({
+            url: `${SERVER_URL}/shops`,
+            method: 'get',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${jwtToken}`
+            }
+          });
+          
+          console.log(shopRes);
+          shopAuthentication = true;
+        } catch (error) {
+          console.log('error', error);
+        }
+
+        if (shopAuthentication) {
+          console.log("Shop authenticated successfully!");
+          ctx.redirect("/");
+        } else {
+          console.log('2')
+          ctx.redirect("/login.html")
+        }
       }
     })
   )
+
+  server.use(async (ctx, next) => {
+    console.log(ctx.state.shopify)
+    if (!ctx.state.shopify) {
+      // Redirect to the login page if not authenticated
+      // ctx.redirect('/login');
+      // return;
+      console.log('khong co');
+    } else {
+      console.log('co');
+    }
+
+    await next();
+  });
+
+  router.get('/login', async (ctx) => {
+    console.log('cuong yeu hoa')
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
   const handleRequest = async (ctx) => {
     await handle(ctx.req, ctx.res);
@@ -62,7 +125,7 @@ app.prepare().then(() => {
   router.get("/_next/webpack-hmr", handleRequest);
   router.get(
       "(.*)",
-      verifyRequest({ accessMode: "offline", fallbackRoute: "/login.html" }),
+      verifyRequest({ accessMode: "offline", authRoute: "/auth"}),
       handleRequest,
   );
 
